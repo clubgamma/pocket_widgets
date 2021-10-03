d29a7151afcbc67ec370aa1c92a2dd720cf179ce
Max Walters <max.walters10@gmail.com>
2021-10-03 12:47:42 -0400

    Additions + auto-formatter

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -9,6 +9,7 @@ import 'package:widgets_book/Contributions/sidB67.dart';
 import 'package:widgets_book/Contributions/Krunal3909_custom_button.dart';
 import 'package:widgets_book/Contributions/rounded_textfield_krunal3909.dart';
 import 'package:widgets_book/Contributions/krunal3909_popup.dart';
+import 'package:widgets_book/Contributions/warpaltarpers.dart';
 
 import '../Contributions/club_gamma.dart';
 import '../Widget/home_card.dart';
@@ -19,7 +20,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   List<Widget> _widgets = [];
 
   @override
@@ -28,112 +28,137 @@ class _HomePageState extends State<HomePage> {
     loadWidgets();
   }
 
-  loadWidgets(){
-
+  loadWidgets() {
     List<Widget> cards = [
-
       GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => RushiDonga(contributorName: "rushi_donga", widgetName: "GF Card")));
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => RushiDonga(
+                      contributorName: "rushi_donga", widgetName: "GF Card")));
         },
         child: HomeCard(
           widgetName: "GF Card",
           contributorName: "rushi_donga",
         ),
       ),
-
       GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ClubGamma(contributorName: "club_gamma", widgetName: "List Card")));
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ClubGamma(
+                      contributorName: "club_gamma", widgetName: "List Card")));
         },
         child: HomeCard(
           widgetName: "List Card",
           contributorName: "club_gamma",
         ),
       ),
-       GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const sidB67()));
+      GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const sidB67()));
         },
         child: HomeCard(
           widgetName: "Rate Widget",
           contributorName: "sidB67",
         ),
       ),
-      
-       GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CustomButton(widgetName: "Custom Button")));
+      GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      CustomButton(widgetName: "Custom Button")));
         },
         child: HomeCard(
           widgetName: "Custom Button",
           contributorName: "Krunal3909",
         ),
       ),
-      
       GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => RoundedTextField(contributorName: "krunal3909", widgetName: "RoundedTextField")));
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => RoundedTextField(
+                      contributorName: "krunal3909",
+                      widgetName: "RoundedTextField")));
         },
         child: HomeCard(
           widgetName: "RoundedTextField",
           contributorName: "krunal_3909",
         ),
       ),
-      
       GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Popup(contributorName: "krunal3909", widgetName: "Popup")));
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Popup(
+                      contributorName: "krunal3909", widgetName: "Popup")));
         },
         child: HomeCard(
           widgetName: "Popup",
           contributorName: "krunal_3909",
         ),
-      )
-
+      ),
+      GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Warpaltarpers(
+                      contributorName: "warpaltarpers",
+                      widgetName: "Neumorphic Round Button")));
+        },
+        child: HomeCard(
+          widgetName: "Neumorphic Round Button",
+          contributorName: "warpaltarpers",
+        ),
+      ),
     ];
 
     for (int i = 0; i < cards.length; i++) {
       _widgets.add(cards[i]);
     }
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kMediumBlueColor,
-      appBar: AppBar(
         backgroundColor: kMediumBlueColor,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "Widget's App",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.5
+        appBar: AppBar(
+          backgroundColor: kMediumBlueColor,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "Widget's App",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5),
           ),
         ),
-      ),
-      body: AnimationLimiter(
-        child: ListView.builder(
-          itemCount: _widgets.length,
-          itemBuilder: (BuildContext context, int index) {
-            return AnimationConfiguration.staggeredList(
-              position: index,
-              duration: const Duration(milliseconds: 1000),
-              child: SlideAnimation(
-                verticalOffset: 50.0,
-                child: FadeInAnimation(
-                  child: _widgets[index],
+        body: AnimationLimiter(
+          child: ListView.builder(
+            itemCount: _widgets.length,
+            itemBuilder: (BuildContext context, int index) {
+              return AnimationConfiguration.staggeredList(
+                position: index,
+                duration: const Duration(milliseconds: 1000),
+                child: SlideAnimation(
+                  verticalOffset: 50.0,
+                  child: FadeInAnimation(
+                    child: _widgets[index],
+                  ),
                 ),
-              ),
-            );
-          },
-        ),
-      )
-    );
+              );
+            },
+          ),
+        ));
   }
 }

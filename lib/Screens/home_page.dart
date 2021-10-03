56cc9cb82dff34ead8cfa3138b80ba19386e9fcb
Krunal Suthar <59758940+Krunal3909@users.noreply.github.com>
2021-10-03 14:56:31 +0530

    Update home_page.dart

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,158 +1,139 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:flutter/services.dart';
-import 'package:getwidget/colors/gf_color.dart';
-import 'package:getwidget/components/avatar/gf_avatar.dart';
-import 'package:getwidget/components/button/gf_button.dart';
-import 'package:getwidget/components/button/gf_button_bar.dart';
-import 'package:getwidget/components/card/gf_card.dart';
-import 'package:getwidget/components/list_tile/gf_list_tile.dart';
-import 'package:getwidget/position/gf_position.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:widgets_book/Constants/color.dart';
+import 'package:widgets_book/Contributions/rushi_donga.dart';
+import 'package:widgets_book/Contributions/sidB67.dart';
+import 'package:widgets_book/Contributions/Krunal3909_custom_button.dart';
+import 'package:widgets_book/Contributions/rounded_textfield_krunal3909.dart';
+import 'package:widgets_book/Contributions/popup_krunal3909.dart';
 
-class Popup extends StatefulWidget {
+import '../Contributions/club_gamma.dart';
+import '../Widget/home_card.dart';
+
+class HomePage extends StatefulWidget {
   @override
-  _PopupState createState() => _PopupState();
-
-  final String widgetName;
-  final String contributorName;
-  Popup({Key? key, required this.contributorName, required this.widgetName}) : super(key: key);
+  _HomePageState createState() => _HomePageState();
 }
 
-class _PopupState extends State<Popup> {
+class _HomePageState extends State<HomePage> {
 
-  String code = "";
-
-  fetchAlgoFromFile() async{
-    String algorithm;
-    algorithm = await rootBundle.loadString("assets/files/popup_krunal3909.txt");
-
-    setState(() {
-      code = algorithm;
-    });
-
-    _forgottenPassword();
-  }
-
-  _forgottenPassword(){
-    Alert(
-        context: context,
-        title: "Forgotten Password",
-        content: Column(
-          children: <Widget>[
-            Text(
-              "We will send You the Password Reset Link on the entered E-mail",
-              style: TextStyle(
-                  color: Colors.grey[800],
-                  fontWeight: FontWeight.w500,
-                  letterSpacing: 0.5,
-                  fontSize: 13.5
-              ),
-              textAlign: TextAlign.center,
-            ),
-            TextField(
-              onChanged: (value){},
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  icon: const Icon(
-                    Icons.email,
-                    color: Color(0XFFA4AAEE),
-                  ),
-                  hintText: "E-mail",
-                  hintStyle: TextStyle(
-                      color: Colors.grey[500]
-                  ),
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide.none
-                  )
-              ),
-            ),
-          ],
-        ),
-        buttons: [
-          DialogButton(
-            onPressed: (){},
-            child: const Text(
-              "SEND",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          )
-        ]).show();
-  }
+  List<Widget> _widgets = [];
 
   @override
   void initState() {
     super.initState();
-    fetchAlgoFromFile();
+    loadWidgets();
   }
+
+  loadWidgets(){
+
+    List<Widget> cards = [
+
+      GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => RushiDonga(contributorName: "rushi_donga", widgetName: "GF Card")));
+        },
+        child: HomeCard(
+          widgetName: "GF Card",
+          contributorName: "rushi_donga",
+        ),
+      ),
+
+      GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => ClubGamma(contributorName: "club_gamma", widgetName: "List Card")));
+        },
+        child: HomeCard(
+          widgetName: "List Card",
+          contributorName: "club_gamma",
+        ),
+      ),
+       GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const sidB67()));
+        },
+        child: HomeCard(
+          widgetName: "Rate Widget",
+          contributorName: "sidB67",
+        ),
+      ),
+      
+       GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => CustomButton(widgetName: "Custom Button")));
+        },
+        child: HomeCard(
+          widgetName: "Custom Button",
+          contributorName: "Krunal3909",
+        ),
+      ),
+      
+      GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => RoundedTextField(contributorName: "krunal3909", widgetName: "RoundedTextField")));
+        },
+        child: HomeCard(
+          widgetName: "RoundedTextField",
+          contributorName: "krunal_3909",
+        ),
+      )
+      
+      GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Popup(contributorName: "krunal3909", widgetName: "Popup")));
+        },
+        child: HomeCard(
+          widgetName: "Popup",
+          contributorName: "krunal_3909",
+        ),
+      )
+
+    ];
+
+    for (int i = 0; i < cards.length; i++) {
+      _widgets.add(cards[i]);
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      backgroundColor: kMediumBlueColor,
       appBar: AppBar(
-        centerTitle: true,
         backgroundColor: kMediumBlueColor,
-        title: Text(
-          widget.widgetName,
-          style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.0
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Widget's App",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5
           ),
         ),
       ),
-      body: ListView(
-        children: [
-
-          const SizedBox(height: 10.0,),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Container(
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  color: kLightBlueColor
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                child: Text(
-                  code,
+      body: AnimationLimiter(
+        child: ListView.builder(
+          itemCount: _widgets.length,
+          itemBuilder: (BuildContext context, int index) {
+            return AnimationConfiguration.staggeredList(
+              position: index,
+              duration: const Duration(milliseconds: 1000),
+              child: SlideAnimation(
+                verticalOffset: 50.0,
+                child: FadeInAnimation(
+                  child: _widgets[index],
                 ),
               ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100.0),
-            child: MaterialButton(
-              onPressed: (){
-                Clipboard.setData(ClipboardData(text: code));
-
-                SnackBar snackBar = const SnackBar(
-                    content: Text(
-                        "Copied!"
-                    )
-                );
-                Scaffold.of(context).showSnackBar(snackBar);
-              },
-              color: kMediumBlueColor,
-              child: const Text(
-                "COPY CODE",
-                style: TextStyle(
-                    color: Colors.white
-                ),
-              ),
-            ),
-          )
-
-        ],
-      ),
+            );
+          },
+        ),
+      )
     );
   }
 }

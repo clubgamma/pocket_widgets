c5a2b620c496950a274639fbe5a2b72fc4520055
Sunali Rambukwella <ramasrambukwella@students.nsbm.ac.lk>
2021-10-27 23:45:50 +0530

    home_card updated

--- lib/Widget/home_card.dart
+++ lib/Widget/home_card.dart
@@ -1,255 +1,84 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:widgets_book/Constants/color.dart';
-import 'package:widgets_book/Contributions/FlipCard.dart';
-import 'package:widgets_book/Contributions/dismissibleCard.dart';
-import 'package:widgets_book/Contributions/drawer_mayank905473.dart';
-import 'package:widgets_book/Contributions/modal_bottom_sheet_krunal3909.dart';
-import 'package:widgets_book/Contributions/rgbPicker_hurshh.dart';
-import 'package:widgets_book/Contributions/rushi_donga.dart';
-import 'package:widgets_book/Contributions/sidB67.dart';
-import 'package:widgets_book/Contributions/Krunal3909_custom_button.dart';
-import 'package:widgets_book/Contributions/rounded_textfield_krunal3909.dart';
-import 'package:widgets_book/Contributions/krunal3909_popup.dart';
-import 'package:widgets_book/Contributions/slider_mayank905473.dart';
-import 'package:widgets_book/Contributions/warpaltarpers.dart';
-import 'package:widgets_book/Contributions/welcome_text_sunalii.dart';
 
-import '../Contributions/club_gamma.dart';
-import '../Widget/home_card.dart';
+class HomeCard extends StatelessWidget {
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<Widget> _widgets = [];
-
-  @override
-  void initState() {
-    super.initState();
-    loadWidgets();
-  }
-
-  loadWidgets() {
-    List<Widget> cards = [
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => RushiDonga(
-                      contributorName: "rushi_donga", widgetName: "GF Card")));
-        },
-        child: HomeCard(
-          widgetName: "GF Card",
-          contributorName: "rushi_donga",
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ClubGamma(
-                      contributorName: "club_gamma", widgetName: "List Card")));
-        },
-        child: HomeCard(
-          widgetName: "List Card",
-          contributorName: "club_gamma",
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const sidB67()));
-        },
-        child: HomeCard(
-          widgetName: "Rate Widget",
-          contributorName: "sidB67",
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) =>
-                      CustomButton(widgetName: "Custom Button")));
-        },
-        child: HomeCard(
-          widgetName: "Custom Button",
-          contributorName: "Krunal3909",
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => RoundedTextField(
-                      contributorName: "krunal3909",
-                      widgetName: "RoundedTextField")));
-        },
-        child: HomeCard(
-          widgetName: "RoundedTextField",
-          contributorName: "krunal_3909",
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Popup(
-                      contributorName: "krunal3909", widgetName: "Popup")));
-        },
-        child: HomeCard(
-          widgetName: "Popup",
-          contributorName: "krunal_3909",
-        ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Warpaltarpers(
-                      contributorName: "warpaltarpers",
-                      widgetName: "Neumorphic Round Button")));
-        },
-        child: HomeCard(
-          widgetName: "Neumorphic Round Button",
-          contributorName: "warpaltarpers",
-        ),
-      ),
-      GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ModalBottomSheet(contributorName: "krunal3909", widgetName: "Bottom Sheet")));
-        },
-        child: HomeCard(
-          widgetName: "Modal Bottom Sheet",
-          contributorName: "krunal_3909",
-        ),
-      ),
-         GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => DismissibleCard(
-                      contributorName: "Palak-Bera",
-                      widgetName: "Dismissible Card")));
-        },
-        child: HomeCard(
-          widgetName: "Dismissible Card",
-          contributorName: "Palak-Bera",
-        ),
-      ),
-      
-      GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Drawermayank905473(contributorName: "mayank905473", widgetName: "Drawer")));
-        },
-        child: HomeCard(
-          widgetName: "Drawer ",
-          contributorName: "mayank905473",
-        ),
-      ),
-      GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => rgb_picker(contributorName: "hurshh", widgetName: "RGB Picker")));
-        },
-        child: HomeCard(
-          widgetName: "RGB Picker",
-          contributorName: "hurshh",
-        ),
-      ),
-
-       GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => FlipCardPage(
-                      contributorName: "Palak-Bera", widgetName: "Flip Card")));
-        },
-        child: HomeCard(
-          widgetName: "Flip Card",
-          contributorName: "Palak-Bera",
-        ),
-      ),
-
-       GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Image_slider(
-                      contributorName: "Mayank Chhatraya", widgetName: "Image Slider")));
-        },
-        child: HomeCard(
-          widgetName: "Image Slider",
-          contributorName: "mayank chhatraya",
-        ),
-      ),
-
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => TextGradientPage(contributorName: "Sunali", widgetName: "Text Gradient")));
-        },
-        child: HomeCard(
-          widgetName: "Text Gradient",
-          contributorName: "sunalii",
-        ),
-      ),
-    
-    ];
-
-    for (int i = 0; i < cards.length; i++) {
-      _widgets.add(cards[i]);
-    }
-  }
+  final String widgetName;
+  final String contributorName;
+  HomeCard({Key? key, required this.contributorName, required this.widgetName}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: kMediumBlueColor,
-        appBar: AppBar(
-          backgroundColor: kMediumBlueColor,
-          elevation: 0,
-          centerTitle: true,
-          title: const Text(
-            "Widget's App",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Stack(
+        children: [
+
+          Padding(
+            padding: const EdgeInsets.only(left: 50),
+            child: Container(
+              height: 80.0,
+              width: MediaQuery.of(context).size.width-80,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  color: kDarkBlueColor
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 50.0, top: 13.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "$widgetName",
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          letterSpacing: 1.0
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3.0,
+                    ),
+                    Text(
+                      "@$contributorName",
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15.0,
+                          letterSpacing: 1.0
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
-        body: AnimationLimiter(
-          child: ListView.builder(
-            itemCount: _widgets.length,
-            itemBuilder: (BuildContext context, int index) {
-              return AnimationConfiguration.staggeredList(
-                position: index,
-                duration: const Duration(milliseconds: 1000),
-                child: SlideAnimation(
-                  verticalOffset: 50.0,
-                  child: FadeInAnimation(
-                    child: _widgets[index],
+
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, top: 8.0),
+            child: Container(
+              height: 60.0,
+              width: 60.0,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                color: const Color(0xffdfddee),
+              ),
+              child:  Center(
+                child: Text(
+                  "${widgetName[0]} ${contributorName[0].toUpperCase()}",
+                  style: const TextStyle(
+                      color: kDarkBlueColor,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold
                   ),
                 ),
-              );
-            },
+              ),
+            ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
